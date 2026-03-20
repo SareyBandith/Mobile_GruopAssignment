8ca4fda18ec2c9387163729f8c7b715f7a80e28e
Sareybandith Seang <bandithsarey@gmail.com>
2026-03-20 22:34:11 +1100

    The task have done: - All clicked image button and button in the Activity 1 are done

--- app/src/main/java/com/example/gruopassignment/MainActivity.java
+++ app/src/main/java/com/example/gruopassignment/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gruopassignment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // clicked Barcode Reader Button
+        ImageButton btnBarcode = findViewById(R.id.imageButtonBarcodeReader);
+        btnBarcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BarcodeReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+        //click Content Reader Button
+        ImageButton btncontent = findViewById(R.id.imageButtonContentReader);
+        btncontent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContentReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Clicked Text reader Button
+        ImageButton btnTextReader = findViewById(R.id.imageButtonTextReader);
+        btnTextReader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TextReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+        //clicked List Of Analaysed images
+        Button btnlistOfAnalaysedImages = findViewById(R.id.ButtonListOfAnalaysedImages);
+        btnlistOfAnalaysedImages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
